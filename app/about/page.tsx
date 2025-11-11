651a8a7d76886991c1a7af20a449355d7c932e09
Jeff Hogg <jeffhogg86@gmail.com>
2025-11-11 10:37:18 -0600

    update metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from 'next'
 import AboutMe from '../../components/AboutMe'
 
 export const metadata: Metadata = {
-  title: 'About Jeff Hogg',
-  description: 'Learn about my experience as a software developer, including my work at Cisco on the Webex Control Hub Platform.',
+	title: 'About - Jeff Hogg',
+	description:
+		'Learn about Jeff Hogg, a software developer with enterprise-level experience in Angular, React, TypeScript, and Next.js, including his work at Cisco.',
 }
 
 export default function About() {
-  return <AboutMe />
+	return <AboutMe />
 }

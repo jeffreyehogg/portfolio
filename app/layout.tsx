baa0aa3935bef7bfe63558002a1e49d70c9f4255
Jeff Hogg <jeffhogg86@gmail.com>
2025-11-10 19:22:59 -0600

    migrate upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,48 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import GoogleAnalytics from '../components/GoogleAnalytics';
-import '../styles/globals.css';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import GoogleAnalytics from '../components/GoogleAnalytics'
+import '../styles/globals.css'
+import { Suspense } from 'react'
 
-const inter = Inter({ subsets: ['latin'], display: 'swap' });
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
-  title: 'Jeff Hogg - Software Engineer',
-  description: 'A personal portfolio Website. Built with Next.js and Tailwind',
-  openGraph: {
-    title: 'Jeff Hogg',
-    description: 'A personal portfolio Website. Built with Next.js and Tailwind',
-    url: 'https://jeffhogg.com/',
-    type: 'website',
-    images: [
-      {
-        url: 'https://jeffhogg.com/images/portfolio.png',
-      },
-    ],
-  },
-};
+	title: 'Jeff Hogg - Software Engineer',
+	description: 'A personal portfolio Website. Built with Next.js and Tailwind',
+	openGraph: {
+		title: 'Jeff Hogg',
+		description:
+			'A personal portfolio Website. Built with Next.js and Tailwind',
+		url: 'https://jeffhogg.com/',
+		type: 'website',
+		images: [
+			{
+				url: 'https://jeffhogg.com/images/portfolio.png',
+			},
+		],
+	},
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en" className={inter.className}>
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+	return (
+		<html lang='en' className={inter.className}>
+			<Suspense fallback={null}>
+				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+			</Suspense>
 
-      <body>
-        <div className="flex flex-col h-screen">
-          <Navbar />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+			<body>
+				<div className='flex flex-col h-screen'>
+					<Navbar />
+					<main className='flex-1'>{children}</main>
+					<Footer />
+				</div>
+			</body>
+		</html>
+	)
 }
---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+// app/not-found.tsx
+import Link from 'next/link'
+
+export default function NotFound() {
+	return (
+		<main className='grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8'>
+			<div className='py-36'>
+				<div className='text-center'>
+					<p className='text-sm font-semibold text-indigo-400 uppercase tracking-wide'>
+						404 error
+					</p>
+					<h1 className='mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl'>
+						Wow, you found a page that doesn't exist!
+					</h1>
+					<p className='mt-2 text-base text-gray-500'>
+						How did you even get here? I'm impressed.
+					</p>
+					<div className='mt-6'>
+						<Link
+							href='/'
+							className='text-base font-medium text-indigo-400 hover:text-indigo-500'
+						>
+							Go back home<span aria-hidden='true'> &rarr;</span>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
+}

7c3a61c4d1fd339c3f1333039fcb47b6f81e7b7b
Jeff Hogg <jeffhogg86@gmail.com>
2026-01-25 08:35:12 -0600

    update config

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,11 @@ import Navbar from '../components/layout/Navbar'
 import Footer from '../components/layout/Footer'
 import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'], display: 'swap' })
+const inter = Inter({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
 	title: 'Jeff Hogg - Software Developer',
@@ -29,8 +33,8 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang='en' className={inter.className}>
-			<body className='bg-white'>
+		<html lang='en' className={`${inter.variable}`}>
+			<body className='bg-white font-sans antialiased'>
 				<div className='flex flex-col min-h-screen'>
 					<Navbar />
 					<main className='flex-1 flex flex-col'>{children}</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+				sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				// A modern, deep slate palette for a "professional" dark mode feel if desired
